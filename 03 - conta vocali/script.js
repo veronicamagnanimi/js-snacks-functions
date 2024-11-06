2d59ea50c8306191e21143028ea1242e64847ce3
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-06 16:32:55 +0100

    ex: 3 in progress

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -9,13 +9,25 @@ const word = 'javascript';
  * @return {array} 
  */
 
-function vocali(parola) {
-    const array = parola.split('aeiou');
+function vocali(stringa) {
+    const listaVocali = ['a', 'e', 'i', 'o', 'u'];
+    let conteggio = [];
+    let variabile;
+    let array = stringa.split("");
+
+    for(let i = 0; i < array.length; i++) {
+        variabile = array[i];
+
+        if(listaVocali.includes(variabile)) {
+            conteggio.push(variabile);
+        }
+    }
+    return conteggio;
 }
-console.log(vocali());
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let result = conteggio(word);
+console.log(result);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
