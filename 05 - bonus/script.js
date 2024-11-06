4a19b74d1e32839f3f8f33d6fbee48b1b45aa4e7
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-06 16:43:15 +0100

    feat: add bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,25 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+//Parto da una stringa e mi restituisce una stringa
 
+const saluto = (nome) => {
+    const date = new Date();
+    const ora = date.getHours;
+    let result = '';
+
+    if (ora <= 13) {
+        result += 'Buongiorno'
+    } else if (ora < 17) {
+        result += 'Buon pomeriggio'
+    } else {
+        result += 'Buonasera'
+    }
+     return result += ` ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(saluto(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
