9b6b065f1826a9d63af06e7503d351bbb27a431d
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-06 15:19:00 +0100

    feat: add ex 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,11 +4,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/**
+ * 
+ * @param {string} array 
+ * @return {string} 
+ */
 
-
+function initials(array) {
+    const addInitials = [];  
+    for (let i = 0; i < array.length; i++) {
+      addInitials.push(array[i][0]); 
+    }
+    return addInitials; 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const addInitials = initials(names);
+console.log(addInitials);  
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
